19da0f941a114801bad0495a76b195b985bca08f
Matt Smith <mattsmith@matts-air>
2016-11-13 13:28:37 +0000

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Matt Smith on 13/11/2016.
+//  Copyright © 2016 Matt Blake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
